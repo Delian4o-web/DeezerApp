03b0b2dacae22830d82f45c15dbe25fb996d8e6e
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-23 09:41:52 +0200

    Add Artist Search

---
+++ deezerapp/src/app/services/artists.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { of, Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Artist } from "../models/artist";
+import { InjectableCompiler } from "@angular/compiler/src/injectable_compiler";
+
+@Injectable({
+  providedIn: "root"
+})
+export class ArtistsService {
+  constructor(private http: HttpClient) {}
+
+  getArtists(query?: string): Observable<Artist> {
+    return this.http.get<Artist>(
+      `${environment.apiUri}/api/artist/${query ? "?${query}" : ""}`
+    );
+  }
+}
