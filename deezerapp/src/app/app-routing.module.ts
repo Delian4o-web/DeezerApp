2460675b62510d0386cd6419a3e924d4fea86553
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-24 11:18:30 +0200

    Add Routing

--- deezerapp/src/app/app-routing.module.ts
+++ deezerapp/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ArtistInfoComponent } from "./artist-info/artist-info.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: "artist", component: ArtistInfoComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
