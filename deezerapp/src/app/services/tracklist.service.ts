85706e4c19e77928d192c69662ad8f871db18739
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-24 12:31:53 +0200

    Add Get tracklist method

--- deezerapp/src/app/services/tracklist.service.ts
+++ deezerapp/src/app/services/tracklist.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Tracklist } from "../models/tracklist";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class TracklistService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getTrackList(artistID?: number): Observable<Tracklist> {
+    return this.http.get<Tracklist>(
+      `${environment.apiUri}/artist/${artistID}/top/limit=5`
+    );
+  }
 }
