c3abd71715b99fa90aab1515c85d4820fb08e816
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-24 15:09:03 +0200

    Add Album Service

---
+++ deezerapp/src/app/services/album.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Tracklist } from "../models/tracklist";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+
+@Injectable({
+  providedIn: "root"
+})
+export class AlbumService {
+  constructor(private http: HttpClient) {}
+
+  // getTrackList(artistID?: number): Observable<Tracklist> {
+  //   return this.http.get<Tracklist>(
+  //     `${environment.apiUri}/artist/${artistID}/top/limit=5`
+  //   );
+  // }
+}
