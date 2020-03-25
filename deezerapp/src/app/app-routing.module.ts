bfb0a451a7fd32ca7293732541a55ed533fc5564
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-25 16:35:04 +0200

    Add Artist Searched Component to paths

--- deezerapp/src/app/app-routing.module.ts
+++ deezerapp/src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ArtistInfoComponent } from "./artist-info/artist-info.component";
 import { ArtistSearchComponent } from "./artist-search/artist-search.component";
+import { ArtistSearchedComponent } from "./artist-searched/artist-searched.component";
 
 const routes: Routes = [
   { path: "", component: ArtistSearchComponent },
   { path: "artist/:id", component: ArtistInfoComponent },
-  { path: "search/:name", component: ArtistSearchComponent }
+  { path: "search/:name", component: ArtistSearchedComponent }
 ];
 
 @NgModule({
