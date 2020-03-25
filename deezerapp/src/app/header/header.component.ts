9639d7dabc5584378ee0100428eedb9db7654d42
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-25 14:46:30 +0200

    Header Component Changes

--- deezerapp/src/app/header/header.component.ts
+++ deezerapp/src/app/header/header.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Observable, Subject } from "rxjs";
+import { debounceTime, distinctUntilChanged, switchMap } from "rxjs/operators";
+import { Artist } from "../models/artist";
+import { ArtistsService } from "../services/artists.service";
+import { Artists } from "../models/artists";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent implements OnInit {
+  private searchTerms = new Subject<string>();
 
-  constructor() { }
+  constructor(private artistService: ArtistsService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
