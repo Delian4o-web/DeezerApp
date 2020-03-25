d19590e8a555b3b1ae37060478c44bec334f6ca9
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-25 11:54:27 +0200

    Add Homepage Routing

--- deezerapp/src/app/app-routing.module.ts
+++ deezerapp/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ArtistInfoComponent } from "./artist-info/artist-info.component";
+import { ArtistSearchComponent } from "./artist-search/artist-search.component";
 
-const routes: Routes = [{ path: "artist/:id", component: ArtistInfoComponent }];
+const routes: Routes = [
+  { path: "", component: ArtistSearchComponent },
+  { path: "artist/:id", component: ArtistInfoComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
