e393d2afa108c652811a644c69ab70650c72b269
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-30 11:48:34 +0200

    Pipe changes

--- src/app/pipes/thousand-suff.pipe.ts
+++ src/app/pipes/thousand-suff.pipe.ts
@@ -5,9 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
 })
 export class ThousandSuffPipe implements PipeTransform {
   transform(input: any, args?: any): any {
-    var exp,
-      rounded,
-      suffixes = ["k", "M", "G", "T", "P", "E"];
+    const suffixes = ["k", "M", "G", "T", "P", "E"];
 
     if (Number.isNaN(input)) {
       return null;
@@ -17,7 +15,7 @@ export class ThousandSuffPipe implements PipeTransform {
       return input;
     }
 
-    exp = Math.floor(Math.log(input) / Math.log(1000));
+    const exp = Math.floor(Math.log(input) / Math.log(1000));
 
     return (input / Math.pow(1000, exp)).toFixed(args) + suffixes[exp - 1];
   }
