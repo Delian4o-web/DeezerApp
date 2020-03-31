9f56e4774868c1fc70cb40ab5bc1a21e542e9266
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-03-31 08:59:59 +0200

    Mapping return data for tracklist

--- src/app/services/tracklist.service.ts
+++ src/app/services/tracklist.service.ts
@@ -4,6 +4,7 @@ import { Tracklist } from "../models/tracklist";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Track } from "../models/track";
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: "root"
@@ -12,8 +13,8 @@ export class TracklistService {
   constructor(private http: HttpClient) {}
 
   getTrackList(artistID?: number): Observable<Track[]> {
-    return this.http.get<Track[]>(
-      `${environment.apiUri}/artist/${artistID}/top?limit=5`
-    );
+    return this.http
+      .get<any>(`${environment.apiUri}/artist/${artistID}/top?limit=5`)
+      .pipe(map(x => x.data));
   }
 }
