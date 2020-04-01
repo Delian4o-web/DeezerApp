dadd6fd37dff4cdb231037eb5141ba5eedbcd18c
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-01 12:55:49 +0200

    Remove unused import

--- src/app/services/tracklist.service.ts
+++ src/app/services/tracklist.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
-import { Tracklist } from "../models/tracklist";
 import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Track } from "../models/track";
