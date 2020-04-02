13746afe0089890ef65162e6aed7bbd05486bf02
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-02 10:30:16 +0200

    Add page not found component to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { ArtistInfoComponent } from "./artist-info/artist-info.component";
 import { ArtistSearchComponent } from "./artist-search/artist-search.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const routes: Routes = [
   { path: "", component: ArtistSearchComponent },
   { path: "artist/:id", component: ArtistInfoComponent },
   { path: "search/:searchTerm", component: ArtistSearchComponent },
-  { path: "**", component: ArtistSearchComponent }
+  { path: "**", component: PageNotFoundComponent }
 ];
 
 @NgModule({
