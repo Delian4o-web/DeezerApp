8bdfce71431b9ff64644557cfbecc7bab5d767e6
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-02 09:52:06 +0200

    Add logic for searching

--- src/app/artist-search/search-bar/search-bar.component.ts
+++ src/app/artist-search/search-bar/search-bar.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from "@angular/core";
+import { ArtistsService } from "src/app/services/artists.service";
+import { Subject, Observable } from "rxjs";
+import { Artist } from "src/app/models/artist";
+import {
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  count
+} from "rxjs/operators";
 
 @Component({
   selector: "app-search-bar",
@@ -6,7 +15,21 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./search-bar.component.scss"]
 })
 export class SearchBarComponent implements OnInit {
-  constructor() {}
+  artists$: Observable<Artist[]>;
+  private searchTerms = new Subject<string>();
+  searchText;
 
-  ngOnInit(): void {}
+  constructor(private artistService: ArtistsService) {}
+
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+
+  ngOnInit(): void {
+    this.artists$ = this.searchTerms.pipe(
+      debounceTime(100),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.artistService.searchArtists(term))
+    );
+  }
 }
