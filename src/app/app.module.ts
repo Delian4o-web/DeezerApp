c5835232293cc13f6355979c766b0ba8832a02ae
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-02 10:11:40 +0200

    Add page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ThousandSuffPipe } from "../app/pipes/thousand-suff.pipe";
 import { MinuteSecondsPipe } from "../app/pipes/minute-seconds.pipe";
 import { ServiceWorkerModule } from "@angular/service-worker";
 import { SearchBarComponent } from './artist-search/search-bar/search-bar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { SearchBarComponent } from './artist-search/search-bar/search-bar.compon
     ArtistInfoComponent,
     ThousandSuffPipe,
     MinuteSecondsPipe,
-    SearchBarComponent
+    SearchBarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
