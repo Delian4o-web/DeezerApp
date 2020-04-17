feafdcc24a14311df1cf0f85afbe03238f3ece38
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-17 14:08:51 +0200

    Add return value test

--- src/app/pipes/thousand-suff.pipe.spec.ts
+++ src/app/pipes/thousand-suff.pipe.spec.ts
@@ -11,4 +11,22 @@ describe("ThousandSuffPipe", () => {
     const input = 900;
     expect(pipe.transform(input)).toEqual(input);
   });
+
+  it("should return thousand value in specific format", () => {
+    const input = 12500;
+
+    expect(pipe.transform(input)).toEqual("13k");
+  });
+
+  it("should return 100 thousand value in specific format", () => {
+    const input = 164680;
+
+    expect(pipe.transform(input)).toEqual("165k");
+  });
+
+  it("should return million value in specific format", () => {
+    const input = 3091095;
+
+    expect(pipe.transform(input)).toEqual("3M");
+  });
 });
