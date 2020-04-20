95d9c10990d3233475786bb019fbec06c3ba6d1f
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-20 09:32:49 +0200

    Add Testing for Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,15 @@ import { ArtistInfoComponent } from "./artist-info/artist-info.component";
 import { ArtistSearchComponent } from "./artist-search/artist-search.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-const routes: Routes = [
+export const routes: Routes = [
   { path: "", component: ArtistSearchComponent },
   { path: "artist/:id", component: ArtistInfoComponent },
   { path: "search/:searchTerm", component: ArtistSearchComponent },
-  { path: "**", component: PageNotFoundComponent }
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,41 @@
-import { TestBed, async } from "@angular/core/testing";
+import { TestBed, fakeAsync, tick } from "@angular/core/testing";
 import { RouterTestingModule } from "@angular/router/testing";
+import { Router, ROUTES } from "@angular/router";
+import { Location } from "@angular/common";
 import { AppComponent } from "./app.component";
+import { ArtistInfoComponent } from "./artist-info/artist-info.component";
+import { ArtistSearchComponent } from "./artist-search/artist-search.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { routes } from "./app-routing.module";
 
 describe("AppComponent", () => {
-  beforeEach(async(() => {
+  let location: Location;
+  let router: Router;
+  let fixture;
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
-    }).compileComponents();
-  }));
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [
+        AppComponent,
+        ArtistInfoComponent,
+        ArtistSearchComponent,
+        PageNotFoundComponent,
+      ],
+    });
+
+    router = TestBed.get(Router);
+    location = TestBed.get(Location);
+
+    fixture = TestBed.createComponent(AppComponent);
+    router.initialNavigation();
+  });
+
+  describe("Routing", () => {
+    it("navigate to 'artist' redirects you to /artist", fakeAsync(() => {
+      router.navigate(["artist"]);
+      tick();
+      expect(location.path()).toBe("/artist");
+    }));
+  });
 });
