aa01dc06bcfa0f8917dddae0da3dbc923428ff8d
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-20 09:40:18 +0200

    Fix Linting Errors

--- src/app/services/tracklist.service.spec.ts
+++ src/app/services/tracklist.service.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { TracklistService } from './tracklist.service';
+import { TracklistService } from "./tracklist.service";
 
-describe('TracklistService', () => {
+describe("TracklistService", () => {
   let service: TracklistService;
 
   beforeEach(() => {
