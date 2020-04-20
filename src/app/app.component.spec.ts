40dcae8a2583152dc2986508baee3acfa65005b0
Delian4o <dgeorgiev@jhb.dvt.co.za>
2020-04-20 09:58:39 +0200

    Linting changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,8 +24,8 @@ describe("AppComponent", () => {
       ],
     });
 
-    router = TestBed.get(Router);
-    location = TestBed.get(Location);
+    router = TestBed.inject(Router);
+    location = TestBed.inject(Location);
 
     fixture = TestBed.createComponent(AppComponent);
     router.initialNavigation();
